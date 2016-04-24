8aec3ca8d5ec2ad35a2a9b2b38c763e9850fb0ca
John Hogarty <hogihung@gmail.com>
2016-04-24 17:02:22 -0400

    Update provision script to install git, gpg key for rvm, curl, rvm and ruby 2.2.3

--- provision.sh
+++ provision.sh
@@ -3,3 +3,11 @@ apt-get -y update
 apt-get -y install nginx
 
 service nginx start
+
+apt-get -y install git
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+apt-get -y install curl
+
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.3
