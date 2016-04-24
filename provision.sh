4d95f1c9b2b68a85f97327a43a02ea90b11e2f45
John Hogarty <hogihung@gmail.com>
2016-04-24 18:16:14 -0400

    Update provision.sh script to add install for bundler, rails 4.2.5 and postgres 9.1

--- provision.sh
+++ provision.sh
@@ -11,3 +11,9 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 apt-get -y install curl
 
 \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.3
+
+gem install bundler
+
+gem install rails -v 4.2.5
+
+sudo apt-get -y install postgresql postgresql-contrib
