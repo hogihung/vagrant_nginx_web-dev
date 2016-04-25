95d95cb6ee1a160e4159b796a755e62ba0d8ca46
John Hogarty <hogihung@gmail.com>
2016-04-25 11:49:01 -0400

    Update provisioning file to include install for vim and postgresql version 9.5.  Update to README to capture these changes.

--- README.md
+++ README.md
@@ -22,18 +22,23 @@ Confirming the basics have all been installed properly:
 
 ```
 vagrant ssh
+
+vagrant@web-dev:~$ uname -a
+Linux web-dev 3.2.0-23-generic #36-Ubuntu SMP Tue Apr 10 20:39:51 UTC 2012 x86_64 x86_64 x86_64 GNU/Linux
+vagrant@web-dev:~$ vim --version
+VIM - Vi IMproved 7.3 (2010 Aug 15, compiled May  4 2012 04:25:35)
+{--snip--}
 vagrant@web-dev:~$ git --version
 git version 1.7.9.5
-vagrant@web-dev:~$ rvm -v
-rvm 1.27.0 (latest) by Wayne E. Seguin <wayneeseguin@gmail.com>, Michal Papis <mpapis@gmail.com> [https://rvm.io/]
 vagrant@web-dev:~$ ruby -v
 ruby 2.2.3p173 (2015-08-18 revision 51636) [x86_64-linux]
 vagrant@web-dev:~$ rails -v
 Rails 4.2.5
-vagrant@web-dev:~$ psql --version
-psql (PostgreSQL) 9.1.21
-contains support for command-line editing
-vagrant@web-dev:~$
+vagrant@web-dev:~$ sudo su postgres -
+postgres@web-dev:/home/vagrant$ psql --version
+psql (PostgreSQL) 9.5.2
+postgres@web-dev:/home/vagrant$ exit
+➜  vagrant_nginx git:(master) ✗
 ```
 
 
--- provision.sh
+++ provision.sh
@@ -1,19 +1,39 @@
+# Update Ubuntu
 apt-get -y update
 
+# Install the Nginx web server
 apt-get -y install nginx
 
+# Start nginx
 service nginx start
 
+# Install Vim
+apt-get -y install vim
+
+# Install git tool
 apt-get -y install git
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-
+# Install curl utility
 apt-get -y install curl
+
+# Install RVM (Ruby Version Manager)
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
 \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.2.3
 
+source /usr/local/rvm/scripts/rvm
+
+# Install Bundler
 gem install bundler
 
+# Install Rails 4.2.5
 gem install rails -v 4.2.5
 
-sudo apt-get -y install postgresql postgresql-contrib
+# Add postgresql 9.5 repo
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
+sudo apt-get -y update
+
+# Install postgresql 9.5
+apt-get -y install postgresql postgresql-contrib
+
